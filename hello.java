9d7ea5209635eb88c7f61d958143666f2d26c82e
kopo06 <hennifer1111@gmail.com>
2023-04-04 10:56:49 +0900

    출력 문장 늘림~~ 수정 할 때마다 커밋 하지 않은 변경사항 이 뜨고 늘 저장해야함

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public class Main () {
         system.out.println ("hello world");
+        system.out.println ("hello world");
+        system.out.println ("hello world");
+        system.out.println ("hello world");
+        system.out.println ("hello world");
     }
 }
